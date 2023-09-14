b34d6053c5b1cb1a73ac065f204f51e2d0e8586f
UserNombre <pablo.esteban.baquero@gmail.com>
2023-09-14 20:57:34 +0200

    [IMP] Show last modified date on posts

---
+++ _plugins/last-modified-hook.rb
@@ -0,0 +1,8 @@
+Jekyll::Hooks.register :posts, :post_init do |post|
+  ncommits = `git rev-list --count HEAD "#{ post.path }"`
+
+  if ncommits.to_i > 1
+    last_modified = `git log -n 1 --pretty="%ad" --date=short "#{ post.path }"`.strip
+    post.data['last_modified'] = last_modified
+  end
+end
