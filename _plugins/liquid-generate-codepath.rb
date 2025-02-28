7f8af90188f47581b4cddb9f0738fa20048ceac0
UserNombre <pabloestebanbaquero@gmail.com>
2025-02-28 23:39:48 +0100

    [IMP] _plugins/liquid-generate-codepath.rb
    Improved error handling and add debugging information

--- _plugins/liquid-generate-codepath.rb
+++ _plugins/liquid-generate-codepath.rb
@@ -4,12 +4,17 @@ module Jekyll
     attr_accessor :definitions
     attr_accessor :tab_width
     attr_accessor :last_repository
+    attr_accessor :debug
 
-    def generate_codepath(yaml, name, width)
+    def generate_codepath(name, yaml, width)
       self.repositories = yaml["repositories"]
       self.definitions = yaml["definitions"]
       self.tab_width = width
       self.last_repository = nil
+      self.debug = ENV["JEKYLL_ENV"] == "development"
+      if self.debug
+        puts "Generating codepath for #{name}"
+      end
       inner = generate_lines(nil, yaml["codepaths"][name], 0)
       html = "<div class='highlighter-rouge unselectable'><pre><code>#{inner}</code></pre></div>"
       return html
@@ -17,17 +22,32 @@ module Jekyll
 
     def generate_lines(parent, nodes, depth)
       html = ""
-      nodes.each do |node|
+      nodes&.each do |node|
+        if self.debug
+          id = node&.dig("call") || node&.dig("code")
+          puts "Generating line for #{id}"
+        end
         line = generate_line(parent, node, depth)
+        if line == nil
+          return nil
+        end
         html += "#{line}\n"
         if node.key?("children")
-          html += generate_lines(node, node["children"], depth + 1)
+          lines = generate_lines(node, node["children"], depth + 1)
+          if lines == nil
+            return nil
+          end
+          html += lines
         end
       end
       return html
     end
 
     def generate_line(parent, node, depth)
+      if node.key?("call") && self.definitions[node["call"]] == nil
+        puts "Invalid key #{node["call"]}"
+        return nil
+      end
       html = ""
       html += generate_indentation(parent, node, depth)
       html += generate_code(parent, node)
@@ -53,11 +73,11 @@ module Jekyll
 
     def generate_code(parent, node)
       html = ""
-      if node.key?("call")
-        definition = self.definitions[node["call"]]
-        html = definition.key?("symbol") ? definition["symbol"] : node["call"]
-      elsif node.key?("code")
+      if node.key?("code")
         html = node["code"]
+      elsif node.key?("call")
+        definition = self.definitions[node["call"]]
+        html = definition.dig("symbol") || node["call"]
       end
       html = "<span class='selectable'>#{html}</span>"
       # FIXME: comment/footnotes style
@@ -80,6 +100,7 @@ module Jekyll
           .gsub("${file}", definition["file"])
           .gsub("${line}", definition["line"].to_s)
         path = definition["file"]
+        # TODO: check parent repository instead of last
         if self.last_repository != definition["repository"]
           path += "@#{definition["repository"]}"
         end
